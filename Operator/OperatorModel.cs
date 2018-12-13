68097247ac917f3c01c147f166548162b2defbda
NaylorLeading <cnaylor@163.com>
2018-12-13 17:10:56 +0800

    first

---
+++ Operator/OperatorModel.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace LeadingCode
+{
+    public class OperatorModel
+    {
+        public string UserId { get; set; }
+        public string UserCode { get; set; }
+        public string UserName { get; set; }
+        public string UserPwd { get; set; }
+        public string CompanyId { get; set; }
+        public string DepartmentId { get; set; }
+        public string RoleId { get; set; }
+        public string LoginIPAddress { get; set; }
+        public string LoginIPAddressName { get; set; }
+        public string LoginToken { get; set; }
+        public DateTime LoginTime { get; set; }
+        public bool IsSystem { get; set; }
+        public bool IsSiteAdmin { get; set; }
+        public bool IsNewsAdmin { get; set; }
+    }
+}
